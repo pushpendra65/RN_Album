40355dbc9c5842c672e790beebfb57e85e25ec80
Pushpendra Khandelwal <pushpendra@Pushpendra.local>
2018-07-09 14:12:22 +0530

    Album header component created

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Import a Library to help create a component
 import React from 'react';
-import { Text,AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component
-const App = () => (<Text> Create A component </Text>)
+const App = () => ( <Header/>)
 
 // Render it to device
 AppRegistry.registerComponent('album', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+// Import Libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => {
+    return <Text>Albums!</Text>;
+};
+
+export default Header;
