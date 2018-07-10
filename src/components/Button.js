ebecf2018d43d6a0e4787c4a1d21d5f70c05aad3
pushpendra65 <pushpendra.khandelwal@kuliza.com>
2018-07-10 13:09:14 +0530

    Custom Button and Linking element

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
-const Button = (props) => {
-    console.log("Card");
-    
+const Button = ( { onPress} ) => {
+    const { textStyle, buttonStyle } = styles
     return(
-         <Text> Click Me!! </Text>
+        <TouchableOpacity onPress = { onPress } style = { buttonStyle} >
+            <Text style = { textStyle }>
+                 Click Me!! 
+            </Text>
+        </TouchableOpacity>    
+         
     )
 
 }
 
 const styles = {
-    containerStyle: {
+
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderColor: '#007aff',
+        borderWidth: 1,
+        marginLeft: 5,
+        marginRigth: 5,
     },
 }
 
